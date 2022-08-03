38420d727b34b60735c09c44f8d999d75a92eda9
Akriti Jha <jhaakriti.26@gmail.com>
2022-08-04 00:16:59 +0530

    startOfLoginPage

---
+++ lib/pages/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class login extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+        
+      ),
+
+    );
+    throw UnimplementedError();
+  }
+
+}
