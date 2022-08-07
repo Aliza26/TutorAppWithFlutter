4081c0ccb6733cfb224df782df40702bdb233ffc
Akriti Jha <jhaakriti.26@gmail.com>
2022-08-07 23:37:12 +0530

    loginPage

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,17 +1,41 @@
+
+
 import 'package:flutter/material.dart';
 
 class login extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Page"),
-        
-      ),
-
+    
+    return  Material(
+      child: Column(
+        children: [Image.asset("assets/images/tutor.jpg",fit:BoxFit.fitHeight,
+        height: 300,),
+        const SizedBox(
+          height: 20,
+        ),
+        const Text("login or signup here",textScaleFactor: 2,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            hintText: "Enter UserName",
+            labelText: "User name",
+          ),),
+          const SizedBox(
+            height: 20,
+          ),
+        TextFormField(
+          decoration:const InputDecoration(
+            hintText: "Enter Your Password",
+            labelText: "Password",
+          ),
+        ),
+        ],
+       
+      )
+      
     );
-    throw UnimplementedError();
   }
-
 }
